fe5d0593605d30c864472f32a473d18e02973c1b
Patrick Burke <zclpatrick@gmail.com>
2026-09-01 22:06:38 +1000

    chore: prepare Android 0.3.1

--- app/composeApp/build.gradle.kts
+++ app/composeApp/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         applicationId = "app.tau"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.3.1"
     }
 
     signingConfigs {
