a6afb66379e97e00be7fb4e723b5838e397736e1
Patrick Burke <zclpatrick@gmail.com>
2026-09-06 01:56:40 +1000

    chore: prepare Tau 0.4.8

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "app.tau"
         minSdk = 26
         targetSdk = 37
-        versionCode = 15
-        versionName = "0.4.7"
+        versionCode = 16
+        versionName = "0.4.8"
     }
 
     signingConfigs {
